6386ef77a7f4e6232b2cab3fcbec5039548a663e
Eduardo Ortega <ortegon000@hotmail.com>
2017-01-27 00:15:52 -0600

    edit readme.md

---
+++ tests/features/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class ExampleTest extends FeatureTestCase
+{
+    function test_basic_example()
+    {
+        $user = factory(\App\User::class)->create([
+            'name' => 'Eduardo Ortega',
+            'email' => 'ortegon000@hotmail.com'
+        ]);
+        $this->actingAs($user, 'api');
+        $this->visit('api/user')
+             ->see('Eduardo Ortega')
+             ->see('ortegon000@hotmail.com');
+    }
+}
