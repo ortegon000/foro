25d71b036716383249d118fcfdb58e121480be40
Eduardo Ortega <ortegon000@hotmail.com>
2017-01-26 19:38:54 -0600

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user = factory(\App\User::class)->create([
+            'name' => 'Eduardo Ortega',
+            'email' => 'ortegon000@hotmail.com'
+        ]);
+        $this->actingAs($user, 'api');
+        $this->visit('api/user')
+             ->see('Eduardo Ortega')
+             ->see('ortegon000@hotmail.com');
+    }
+}
