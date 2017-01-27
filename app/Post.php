f8eec91a7ba9e849065689f494bac6f338b12464
Eduardo Ortega <ortegon000@hotmail.com>
2017-01-27 15:12:56 -0600

    show post

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title', 'content'
+        'title', 'content', 'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/public.php
+++ routes/public.php
@@ -11,6 +11,7 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +19,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('posts/{post}', [
+    'as' => 'posts.show',
+    'uses' => 'PostController@show'
+])->where('post', '\d+');
